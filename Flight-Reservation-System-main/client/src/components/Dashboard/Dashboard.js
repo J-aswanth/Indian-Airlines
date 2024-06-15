fe3d47f04d7373a8e39b14f884c8aaac4f6bdf73
Jaswanth Malla <128897545+J-aswanth@users.noreply.github.com>
2024-06-16 00:54:55 +0530

    Updated

--- Flight-Reservation-System-main/client/src/components/Dashboard/Dashboard.js
+++ Flight-Reservation-System-main/client/src/components/Dashboard/Dashboard.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import NavBar from "../NavBar/NavBar";
 import AddFlightBtn from "./components/AddFlightBtn";
 import FlightDetails from "./components/FlightDetails";
 import { useLocation } from "react-router-dom";
@@ -9,7 +8,13 @@ const Dashboard = () => {
   const { adminId } = state; // Read values passed on state
   return (
     <Container>
-      <NavBar />
+         <nav className="navbar navbar-expand-lg navbar-light ">
+         <a className="navbar-brand" href="/">
+            <span className="fast">INDIAN </span>{" "}
+            <span className="airlines">AIRLINES</span>
+        </a>
+      </nav>  
+      <hr/>
        <h4 className="text-center text-danger mt-2">Welcome to {adminId} Dashboard! </h4>
       <Row className="mt-5">
         <Col>
